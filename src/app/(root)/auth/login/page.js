50ccfd2e46171f1598948a2ffb462079415ed0c5
Farooqfar <info4895@gmail.com>
2025-11-23 20:58:14 +0500

    all set login

--- src/app/(root)/auth/login/page.js
+++ src/app/(root)/auth/login/page.js
@@ -1,11 +1,27 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function page() {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+  const handlelogin = (e) => {
+    let { name, value } = e.target;
+    setLogin((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(login);
+  };
   return (
     <>
       <section className="w-full h-screen flex justify-center items-center flex-col">
-        <form className="w-[500px] h-100 bg-gray-800 flex flex-col">
+        <form
+          className="w-[500px] h-100 bg-gray-800 flex flex-col"
+          onSubmit={handleForm}
+        >
           <h1 className="text-[#E1380A] text-2xl text-bold text-center p-3 font-bold">
             Login
           </h1>
@@ -13,14 +29,22 @@ export default function page() {
             <label className="text-gray-400">Email</label>
             <input
               type="email"
-              className="border p-1 caret-[#E1380A] text-[#E1380A] outline-none"
+              className="border p-1 caret-[#E1380A] text-[#E1380A] outline-none placeholder-[#E1380A]"
+              placeholder="Email"
+              name="email"
+              value={login.email}
+              onChange={(e) => handlelogin(e)}
             />
           </div>
           <div className="w-full flex flex-col p-5">
-            <label className="text-gray-400">Email</label>
+            <label className="text-gray-400">Password</label>
             <input
               type="password"
-              className="border p-1 caret-[#E1380A] text-[#E1380A] outline-none"
+              className="border p-1 caret-[#E1380A] text-[#E1380A] outline-none placeholder-[#E1380A]"
+              placeholder="Password"
+              name="password"
+              value={login.password}
+              onChange={(e) => handlelogin(e)}
             />
           </div>
           <div className="w-full flex flex-col p-5">
