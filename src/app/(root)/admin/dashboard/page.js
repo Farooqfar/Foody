b52462b6a9e5d6681e75201ce275a0776a2b21c5
Farooqfar <info4895@gmail.com>
2025-12-02 20:56:34 +0500

    admin

--- src/app/(root)/admin/dashboard/page.js
+++ src/app/(root)/admin/dashboard/page.js
@@ -1,4 +1,5 @@
 import Admin_Nav from "@/app/components/Admin_Nav";
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
@@ -9,19 +10,121 @@ export default function page() {
         <div className="w-full flex justify-end items-center p-8">
           <button className="w-20 bg-amber-600">Add + </button>
         </div>
-        <div>
-          <table>
+        <div className="w-full flex justify-center items-center p-10">
+          <table class="w-full border-separate border-spacing-y-2">
             <thead>
-              <tr>
-                <td className="border">No</td>
-                <td>Name</td>
-                <td>Price</td>
-                <td>Sale</td>
-                <td>Active/Not</td>
-                <td>Edit</td>
-                <td>Delete</td>
+              <tr className="bg-amber-600">
+                <th>No</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Sale</th>
+                <th>Active</th>
+                <th>Edit</th>
+                <th>Delete</th>
               </tr>
             </thead>
+            <tbody className="mt-2">
+              <tr className="odd:bg-gray-800 text-center">
+                <td className="p-2">1</td>
+                <td className="p-2">Pizza</td>
+                <td className="p-2">1000</td>
+                <td className="p-2">sale</td>
+                <td className="p-2">
+                  <button>deactive</button>
+                </td>
+                <td className="p-2">
+                  <Link
+                    href="#"
+                    className="cursor-pointer text-blue-600 underline"
+                  >
+                    edit
+                  </Link>
+                </td>
+                <td className="p-2 text-red-600 underline">
+                  <Link href="#">delete</Link>
+                </td>
+              </tr>
+              <tr className="odd:bg-gray-800 text-center">
+                <td className="p-2">1</td>
+                <td className="p-2">Pizza</td>
+                <td className="p-2">1000</td>
+                <td className="p-2">sale</td>
+                <td className="p-2">
+                  <button>deactive</button>
+                </td>
+                <td className="p-2">
+                  <Link
+                    href="#"
+                    className="cursor-pointer text-blue-600 underline"
+                  >
+                    edit
+                  </Link>
+                </td>
+                <td className="p-2 text-red-600 underline">
+                  <Link href="#">delete</Link>
+                </td>
+              </tr>
+              <tr className="odd:bg-gray-800 text-center">
+                <td className="p-2">1</td>
+                <td className="p-2">Pizza</td>
+                <td className="p-2">1000</td>
+                <td className="p-2">sale</td>
+                <td className="p-2">
+                  <button>deactive</button>
+                </td>
+                <td className="p-2">
+                  <Link
+                    href="#"
+                    className="cursor-pointer text-blue-600 underline"
+                  >
+                    edit
+                  </Link>
+                </td>
+                <td className="p-2 text-red-600 underline">
+                  <Link href="#">delete</Link>
+                </td>
+              </tr>
+              <tr className="odd:bg-gray-800 text-center">
+                <td className="p-2">1</td>
+                <td className="p-2">Pizza</td>
+                <td className="p-2">1000</td>
+                <td className="p-2">sale</td>
+                <td className="p-2">
+                  <button>deactive</button>
+                </td>
+                <td className="p-2">
+                  <Link
+                    href="#"
+                    className="cursor-pointer text-blue-600 underline"
+                  >
+                    edit
+                  </Link>
+                </td>
+                <td className="p-2 text-red-600 underline">
+                  <Link href="#">delete</Link>
+                </td>
+              </tr>
+              <tr className="odd:bg-gray-800 text-center">
+                <td className="p-2">1</td>
+                <td className="p-2">Pizza</td>
+                <td className="p-2">1000</td>
+                <td className="p-2">sale</td>
+                <td className="p-2">
+                  <button>deactive</button>
+                </td>
+                <td className="p-2">
+                  <Link
+                    href="#"
+                    className="cursor-pointer text-blue-600 underline"
+                  >
+                    edit
+                  </Link>
+                </td>
+                <td className="p-2 text-red-600 underline">
+                  <Link href="#">delete</Link>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </section>
